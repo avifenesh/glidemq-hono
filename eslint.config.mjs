676f16f61ca003858782f91c8383edf8272e6e71
Avi Fenesh <aviarchi1994@gmail.com>
2026-02-27 16:12:36 +0200

    chore: add prettier and eslint config matching glide-mq
    - .prettierrc: singleQuote, trailingComma, 120 width
    - eslint.config.mjs: typescript-eslint + eslint-config-prettier
    - Add typescript-eslint and eslint-config-prettier devDeps

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: ['dist/', 'node_modules/', 'example/'],
+  },
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    files: ['src/**/*.ts', 'tests/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'no-constant-condition': 'off',
+    },
+  },
+  {
+    files: ['tests/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    },
+  },
+);
